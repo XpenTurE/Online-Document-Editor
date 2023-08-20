92aecc192b34ae3e203cc5027d51f11dc151f460
Harman Sharma <sharmaharman17@gmail.com>
2023-08-20 14:01:36 +0530

    First Push

---
+++ Server/index.js
@@ -0,0 +1,42 @@
+import { Server } from "socket.io";
+
+const PORT = 9000;
+
+
+const io = new Server(PORT, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  socket.on('get-document',documentId=>{
+    const data = '';
+    socket.join(documentId);
+    socket.emit('load-document',data)
+
+    socket.on('send-changes', delta=>{
+      socket.broadcast.to(documentId).emit('receive-changes',delta)
+    })
+  })
+    console.log('Connected');
+});
+
+
+
+
+// import {Server} from "socket.io"
+
+// const PORT = 9000;
+
+// const io= new Server(5000,{
+//     cors:{
+//         origin:"http://localhost:3000",
+//         methods:["GET","POST"]
+//     }
+// });
+
+// io.on("connection",socket => {
+//     console.log('Connected')
+// });
